16b2cb13b0039da05a46844fa255bfabe5ab8743
elli7ey <smirn.elysees@gmail.com>
2020-12-21 21:46:04 +0300

    Add Intents and fixed design

--- app/src/main/java/com/example/test_design/ThirdActivity.java
+++ app/src/main/java/com/example/test_design/ThirdActivity.java
@@ -1,13 +1,44 @@
 package com.example.test_design;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class ThirdActivity extends Activity {
 
+public class ThirdActivity extends Activity implements OnClickListener{
+
+    private Button chats_btn, contacts_btn, settings_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_third);
+
+        final Button chats_btn = (Button) findViewById(R.id.chats_btn);
+        final Button contacts_btn = (Button) findViewById(R.id.contacts_btn);
+        final Button settings_btn = (Button) findViewById(R.id.settings_btn);
+
+        chats_btn.setOnClickListener(this);
+        contacts_btn.setOnClickListener(this);
+        settings_btn.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.chats_btn:
+                Intent chats_i = new Intent(ThirdActivity.this, MainActivity.class);
+                startActivity(chats_i);
+                break;
+            case R.id.contacts_btn:
+                Intent contacts_i = new Intent(ThirdActivity.this, ThirdActivity.class);
+                startActivity(contacts_i);
+                break;
+            case R.id.settings_btn:
+                Intent settings_i = new Intent(ThirdActivity.this, SecondActivity.class);
+                startActivity(settings_i);
+                break;
+        }
     }
 }
